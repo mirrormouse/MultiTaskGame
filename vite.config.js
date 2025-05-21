6a9f9bb89cd199b514b4f88487d2d46f314a841f
mirrormouse <kagamidokei@gmail.com>
2025-05-21 19:47:08 +0900

    prepare gh-pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-  base: '/my-app/',   // リポジトリ名と合わせる
+  base: '/MultiTaskGame/',   // リポジトリ名と合わせる
   plugins: [react()],
 })
